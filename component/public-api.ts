e45fc64f566af490ddb895e372060bc1c983be5b
kain <zhangtqx@vip.qq.com>
2022-01-17 21:05:50 +0800

    update

--- component/public-api.ts
+++
@@ -1,5 +0,0 @@
-export * from './media-input/picture.component';
-export * from './media-input/video.component';
-export * from './richtext/richtext.component';
-export * from './richtext/config';
-export * from './cms-component.module';
--- component/richtext/language.ts
+++
@@ -1,5 +0,0 @@
-export default {
-  picture: ['图片素材', 'Picture'],
-  video: ['视频素材', 'Video'],
-  insert: ['插入', 'Insert']
-};
