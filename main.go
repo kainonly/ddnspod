420abed8f070824468849d592b98f312d4f8079e
kain <zhangtqx@vip.qq.com>
2022-03-02 11:26:37 +0800

    update

--- main.go
+++ main.go
@@ -1,16 +1,24 @@
 package main
 
-import "ddnspod/bootstrap"
+import (
+	"ddnspod/bootstrap"
+	"os"
+	"os/signal"
+)
 
 func main() {
 	values, err := bootstrap.SetValues()
 	if err != nil {
 		panic(err)
 	}
+
 	c, err := Schedule(values)
 	if err != nil {
 		panic(err)
 	}
 	c.Start()
-	select {}
+
+	exit := make(chan os.Signal, 1)
+	signal.Notify(exit, os.Interrupt)
+	<-exit
 }
